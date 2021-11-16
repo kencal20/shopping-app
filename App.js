e38a7032112cc4c65f4105a9f811f96c47e116f2
Kenneth Adjei-mensah <kenadjei20@gmail.com>
2021-11-16 11:15:58 +0000

    app js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={{ backgroundColor: "blue" }}>
+        <Text style={{ fontSize: 20, color: "#fff" }}>Pick a photo</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +17,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 5,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
